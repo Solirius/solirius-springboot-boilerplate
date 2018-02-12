5b6e75c42a0747d3d13217fb8d043cd9b86c1014
Alec Doran-Twyford <alec@alectronic.co>
2018-02-12 12:32:27 +0000

    refactor test

--- src/test/java/com/solirius/java/AppTests.java
+++ src/test/java/com/solirius/java/AppTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.solirius.java;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class AppTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
 
+    }
 }
